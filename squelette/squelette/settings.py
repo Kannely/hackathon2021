e765f409a242c56b0d2de73c95c215dcc76d72b5
L0LAD <audrey.denoual@live.fr>
2021-01-05 17:50:09 +0100

    Intégration Vue.js

--- squelette/squelette/settings.py
+++ squelette/squelette/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend.apps.BackendConfig',
-    'frontend.apps.FrontendConfig',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
