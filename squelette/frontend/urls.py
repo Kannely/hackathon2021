6c1dc07467c1b3901c63d8c4ed8290d639b478dc
L0LAD <audrey.denoual@live.fr>
2021-01-06 14:46:59 +0100

    Component dynamic text

--- squelette/frontend/urls.py
+++ squelette/frontend/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='front_index'),
     path('about', views.about, name='front_about'),
+    path('animals', views.animals, name='front_animals'),
+    path('vue', views.vue, name='front_vue'),
 ]
