1c2809c321e93ea764c9a52d14254a1f7e6e5054
kevin-lth <kevin.lethuillier@gmail.com>
2021-01-06 15:46:38 +0100

    Added new base template

--- squelette/frontend/urls.py
+++ squelette/frontend/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='front_index'),
     path('about', views.about, name='front_about'),
+    path('base', views.base, name='front_base'),
 ]
--- squelette/frontend/views.py
+++ squelette/frontend/views.py
@@ -19,3 +19,11 @@ def index(request):
 
 def about(request):
     return HttpResponse("About")
+    
+menu = [
+    { "name": "Index", "class": "fas fa-cat", "url": "front_index" },
+    { "name": "About", "class": "fas fa-dog", "url": "front_about" }
+]
+    
+def base(request):
+    return render(request, "base.html", { "menu": menu })
