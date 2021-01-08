a44f5db68a893a0f3349c3b422cffef4e3197190
Kannely <blu.kannely@gmail.com>
2021-01-08 10:13:20 +0100

    Update views.py

--- squelette/frontend/views.py
+++ squelette/frontend/views.py
@@ -1,27 +1,28 @@
-import random, json
-import urllib.request
-
-from django.http import HttpResponse, JsonResponse
+import random
 from django.shortcuts import render
 
 from backend.models import Cat, Kibble
 
-menu = { "menu": [
-    { "name": "Index", "class": "fas fa-cat", "url": "front_index" },
-    { "name": "About", "class": "fas fa-dog", "url": "front_about" },
-    { "name": "Animals", "class": "fas fa-heart", "url": "front_animals" },
-    { "name": "Vue", "class": "fas fa-fish", "url": "front_vue" },
-    { "name": "Movies", "class": "fas fa-paw", "url": "front_movies" }
+menu = {"menu": [
+    {"name": "Index", "class": "fas fa-cat", "url": "front_index"},
+    {"name": "About", "class": "fas fa-dog", "url": "front_about"},
+    {"name": "Animals", "class": "fas fa-heart", "url": "front_animals"},
+    {"name": "Vue", "class": "fas fa-fish", "url": "front_vue"},
+    {"name": "Movies", "class": "fas fa-paw", "url": "front_movies"}
 ]}
+
 
 def index(request):
     return render(request, "index.html", menu)
 
+
 def about(request):
     return render(request, "about.html", menu)
 
+
 def vue(request):
     return render(request, "vue.html", menu)
+
 
 def animals(request):
     # names = ("Marie", "Berlioz", "Toulouse", "Pistache")
@@ -34,5 +35,6 @@ def animals(request):
     print(Cat.objects.all())
     return render(request, "animals.html", context)
 
+
 def movies(request):
-	return render(request, "movies.html", menu)
+    return render(request, "movies.html", menu)
