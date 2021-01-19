5fd85223b29fb0a6ab65e5d883d8bb5726206e2e
L0LAD <audrey.denoual@live.fr>
2021-01-19 09:46:53 +0100

    Synthesis info générales

---
+++ squelette/frontend/static/components/synthesis.mjs
@@ -0,0 +1,27 @@
+Vue.component('synthesis-component', {
+    template: `
+    <div>
+        <p>{{ label }}</p>
+        <button @click="alert()">Alert</button>
+        <input type="text" v-model="favoriteCat" />
+    </div>
+    `,
+	data: function() {
+	    return {
+		    favoriteCat: 'Berlioz'
+		}
+	},
+	computed: {
+		label() {
+			return this.formatCat(this.favoriteCat)
+		}
+	},
+	methods: {
+	    formatCat(cat) {
+	        return `My favorite cat is ${cat}.`
+	    },
+		alert() {
+			alert(this.formatCat(this.favoriteCat))
+		}
+	}
+})
