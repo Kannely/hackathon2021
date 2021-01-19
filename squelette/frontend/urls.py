87bc03fb0aaf7c74feef98bf9d895b194925caf2
L0LAD <audrey.denoual@live.fr>
2021-01-19 09:14:40 +0100

    Navigation menu

--- squelette/frontend/urls.py
+++ squelette/frontend/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('movies', views.movies, name='front_movies'),
     path('animals', views.animals, name='front_animals'),
     path('vue', views.vue, name='front_vue'),
+    path('chart', views.chart, name='front_chart'),
+
+    path('synthesis', views.synthesis, name='front_synthesis'),
+    path('obligations', views.obligations, name='front_obligations'),
+    path('skills', views.skills, name='front_skills'),
+    path('courses', views.courses, name='front_courses'),
 ]
--- squelette/frontend/views.py
+++ squelette/frontend/views.py
@@ -4,11 +4,17 @@ from django.shortcuts import render
 from backend.models import Cat, Kibble
 
 menu = {"menu": [
-    {"name": "Index", "class": "fas fa-home", "url": "front_index"},
-    {"name": "Bus", "class": "fas fa-bus", "url": "front_bus"},
-    {"name": "Animals", "class": "fas fa-cat", "url": "front_animals"},
-    {"name": "Vue", "class": "fas fa-eye", "url": "front_vue"},
-    {"name": "Movies", "class": "fas fa-star", "url": "front_movies"}
+    # {"name": "Index", "class": "fas fa-home", "url": "front_index"},
+    # {"name": "Bus", "class": "fas fa-bus", "url": "front_bus"},
+    # {"name": "Animals", "class": "fas fa-cat", "url": "front_animals"},
+    # {"name": "Vue", "class": "fas fa-eye", "url": "front_vue"},
+    # {"name": "Movies", "class": "fas fa-star", "url": "front_movies"},
+    # {"name": "Chart", "class": "fas fa-heart", "url": "front_chart"},
+
+    {"name": "Ma fiche", "class": "", "url": "front_synthesis"},    
+    {"name": "Conditions de diplomation", "class": "", "url": "front_obligations"},
+    {"name": "Compétences", "class": "", "url": "front_skills"},
+    {"name": "UEs", "class": "", "url": "front_courses"},
 ]}
 
 
@@ -38,3 +44,27 @@ def animals(request):
 
 def movies(request):
     return render(request, "movies.html", menu)
+
+
+def chart(request):
+    labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+    data = [12, 19, 3, 5, 2, 3]
+
+    context = menu
+    context["labels"] = labels
+    context["data"] = data
+
+    return render(request, 'chart.html', context)
+
+
+def synthesis(request):
+    return render(request, "synthesis.html", menu)
+
+def obligations(request):
+    return render(request, "obligations.html", menu)
+
+def skills(request):
+    return render(request, "skills.html", menu)
+
+def courses(request):
+    return render(request, "courses.html", menu)
