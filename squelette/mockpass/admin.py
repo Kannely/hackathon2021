2c1fa28e4bb56b3797e7a85967905974c53cbefc
kevin-lth <kevin.lethuillier@gmail.com>
2021-01-19 11:54:04 +0100

    More admin print

--- squelette/mockpass/admin.py
+++ squelette/mockpass/admin.py
@@ -1,16 +1,34 @@
 from django.contrib import admin
 from .models import *
 
-class CompetenceAdmin(admin.ModelAdmin):
+class EtudiantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nom', 'prenom')
+
+class PeriodeAdmin(admin.ModelAdmin):
+    list_display = ('code',)
+    
+class FormationAdmin(admin.ModelAdmin):
+    list_display = ('code',)
+    
+class ObligationsAdmin(admin.ModelAdmin):
+    list_display = ('stage', 'etranger', 'ielts', 'lv1', 'lv2')
+
+class TAFAdmin(admin.ModelAdmin):
     list_display = ('code', 'nom')
 
+class CompetenceAdmin(admin.ModelAdmin):
+    list_display = ('code', 'nom')
+    
+class UEAdmin(admin.ModelAdmin):
+    list_display = ('code', 'nom', 'responsable', 'creneau')
+
 # Register your models here.
-admin.site.register(Etudiant)
-admin.site.register(TAF)
-admin.site.register(Periode)
-admin.site.register(Formation)
-admin.site.register(Obligations)
+admin.site.register(Etudiant, EtudiantAdmin)
+admin.site.register(TAF, TAFAdmin)
+admin.site.register(Periode, PeriodeAdmin)
+admin.site.register(Formation, FormationAdmin)
+admin.site.register(Obligations, ObligationsAdmin)
 admin.site.register(SuivreUE)
 admin.site.register(EvalCompetence)
 admin.site.register(Competence, CompetenceAdmin)
-admin.site.register(UE)
+admin.site.register(UE, UEAdmin)
