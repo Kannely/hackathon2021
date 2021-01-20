ee4108c06bd8206d5752100fa0180bdb44b37f25
Kannely <blu.kannely@gmail.com>
2021-01-20 11:36:36 +0100

    Update views.py

--- squelette/backend/views.py
+++ squelette/backend/views.py
@@ -36,10 +36,15 @@ def get_synthesis(request):
     etudiant_data, etudiant_code = __make_json_request__(request, url, fields_only=True)
     if etudiant_code == 200:
         for taf in ['tafA2', 'tafA3']:
-            if etudiant_data[taf] == 'null':
+            if etudiant_data[taf] is None:
                 etudiant_data[taf] = '-'
             else:
                 taf_url = __get_pass_url__(request, 'taf/'+str(etudiant_data[taf]))
                 taf_data, taf_code = __make_json_request__(request, taf_url, fields_only=True)
-                return JsonResponse(taf_data, status=taf_code)
+                if taf_code == 200:
+                    etudiant_data[taf] = taf_data['code']
+        formation_url = __get_pass_url__(request, 'formation/'+str(etudiant_data['formation']))
+        formation_data, formation_code = __make_json_request__(request, formation_url, fields_only=True)
+        if formation_code == 200:
+            etudiant_data['formation'] = formation_data['code']
     return JsonResponse(etudiant_data, status=etudiant_code)
