1ea19e1cba4fb10e2a6ecf6dd799c7019f4bb737
kevin-lth <kevin.lethuillier@gmail.com>
2021-01-20 15:53:57 +0100

    Progress on SSO

--- squelette/mocksso/urls.py
+++ squelette/mocksso/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='sso_index'),
+    path('login', views.do_login, name='sso_login'),
+    path('user', views.get_user, name='sso_get_user'),
+    path('logout', views.do_logout, name='sso_logout'),
 ]
--- squelette/mocksso/views.py
+++ squelette/mocksso/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. This is the SSO !")
+
+"""
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+"""
+
+def do_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = serializers.serialize('json', [user], fields=["username"])
+            return HttpResponse(response, status=200)
+        else:
+            return HttpResponse(status=401)
+    return HttpResponse(status=405)
+
+def get_user(request):
+    if request.user.is_authenticated:
+        response = serializers.serialize('json', [request.user], fields=["username"])
+        return HttpResponse(response, status=200)
+    else:
+        return JsonResponse({}, status=404)
+    
+def do_logout(request):
+    logout(request)
+    return HttpResponse(status=200)
+    
