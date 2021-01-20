ab20e6af0d111f13a6d89771be34789c60e039dc
Kannely <blu.kannely@gmail.com>
2021-01-20 11:31:00 +0100

    Update views.py

--- squelette/backend/views.py
+++ squelette/backend/views.py
@@ -34,4 +34,12 @@ def __make_json_request__(request, url, transfer_cookie=True, fields_only=False)
 def get_synthesis(request):
     url = __get_pass_url__(request, 'etudiant')
     etudiant_data, etudiant_code = __make_json_request__(request, url, fields_only=True)
+    if etudiant_code == 200:
+        for taf in ['tafA2', 'tafA3']:
+            if etudiant_data[taf] == 'null':
+                etudiant_data[taf] = '-'
+            else:
+                taf_url = __get_pass_url__(request, 'taf/'+str(etudiant_data[taf]))
+                taf_data, taf_code = __make_json_request__(request, taf_url, fields_only=True)
+                return JsonResponse(taf_data, status=taf_code)
     return JsonResponse(etudiant_data, status=etudiant_code)
