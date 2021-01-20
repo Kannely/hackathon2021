3c6285da36204151107bdd17a06142e43add94b7
kevin-lth <kevin.lethuillier@gmail.com>
2021-01-20 09:04:02 +0100

    Update views.py

--- squelette/mockpass/views.py
+++ squelette/mockpass/views.py
@@ -8,18 +8,19 @@ from .models import *
 
 # Create your views here.
 
-
 def index(request):
     return HttpResponse("Hello, world. This is PASS !")
 
+def __get_etudiant_from_request__(request):
+    _id = request.user.pk
+    if request.user.is_authenticated:
+        etudiant = Etudiant.objects.filter(user.pk==_id)
+        if etudiant:
+            return etudiant
+    return None
 
 def get_etudiant(request):
-    id = request.user.pk
-    if request.user.is_authenticated:
-        response = Etudiant.objects.get(user=id)
-        response = serializers.serialize('json', [response,])
-    else:
-        response = ''
-    return JsonResponse(response, safe=False)
-
-
+    etudiant = __get_etudiant_from_request__(request)
+    if etudiant:
+        response = 
+        return JsonResponse()
