3824f6466afd05978a5e432d95e350938452d64f
Kannely <blu.kannely@gmail.com>
2021-01-20 09:03:05 +0100

    404

--- squelette/mockpass/views.py
+++ squelette/mockpass/views.py
@@ -4,22 +4,21 @@ import urllib.request
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 from .models import *
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
-
-
 def index(request):
     return HttpResponse("Hello, world. This is PASS !")
 
 
 def get_etudiant(request):
     id = request.user.pk
+    response = HttpResponse(status=404)
     if request.user.is_authenticated:
-        response = Etudiant.objects.get(user=id)
+        response = get_object_or_404(Etudiant, user=id)
         response = serializers.serialize('json', [response,])
-    else:
-        response = ''
-    return JsonResponse(response, safe=False)
+        response = JsonResponse(response, safe=False)
+    return response
 
 
